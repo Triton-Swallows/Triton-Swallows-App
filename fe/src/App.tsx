4230bae3aac76795dd423ca8859cff889348aaee
NatsumiMiyamotoJeraTr <Jera.hq.devtr.mem010@outlook.jp>
2026-03-17 13:41:50 +0900

    fix: viteバージョンを7系にダウン&tailwind, chadcnの導入

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -1,12 +1,14 @@
 import { RiHome3Line } from "react-icons/ri";
+import { Button } from "@/components/ui/button";
 import "./App.css";
 
 function App() {
   return (
     <>
-      <div className="home-icon">
+      <div className="flex justify-center items-center h-screen text-[10rem]">
         <RiHome3Line />
       </div>
+      <Button variant="outline">Button Example</Button>
     </>
   );
 }
