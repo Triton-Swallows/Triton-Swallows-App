3e3ef73cc8329ba18762f3f69630f61e59874b58
NatsumiMiyamotoJeraTr <Jera.hq.devtr.mem010@outlook.jp>
2026-03-18 10:55:45 +0900

    create: db: userテーブルmigrationを作成
    Co-authored-by: NaokiMiyamotoJeraTr <NaokiMiyamotoJeraTr@users.noreply.github.com>
    Co-authored-by: kei-kokubu <kei-kokubu@users.noreply.github.com>

---
+++ be/db/migrations/20260318012418_create_user_table.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.createTable("users", function (table) {
+    table.string("uid").primary(); // firebaseuidが主キー
+    table.string("email").unique().notNullable();
+    table.timestamps(true, true);
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.dropTable("users");
+};
