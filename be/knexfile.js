6157bc43d3e17e70228185c74fe4421a413368f9
NatsumiMiyamotoJeraTr <Jera.hq.devtr.mem010@outlook.jp>
2026-03-19 10:16:29 +0900

    fix: knexfileのssl認証まわりの書き方を変更

--- be/knexfile.js
+++ be/knexfile.js
@@ -4,8 +4,9 @@ const DB_USER = process.env.DB_USER;
 const DB_NAME = process.env.DB_NAME;
 const DB_HOST = "127.0.0.1";
 const DB_PORT = "5432";
-const DB_URL = process.env.DB_URL;
 const DB_PASSWORD = process.env.DB_PASSWORD;
+const DB_URL = process.env.DB_URL; // heroku上だけの環境変数
+const DB_SSL = process.env.DB_SSL; // heroku上だけの環境変数
 
 module.exports = {
   development: {
@@ -28,7 +29,7 @@ module.exports = {
   production: {
     client: "postgresql",
     connection: DB_URL,
-    ssl: { rejectUnauthorized: false },
+    ssl: DB_SSL ? { rejectUnauthorized: false } : false,
     migrations: {
       directory: "./db/migrations",
     },
