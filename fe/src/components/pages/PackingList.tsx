57bb13596d5bcca604025d4d94746ec1ff264214
keikokubu <k.kokubu@scsk.jp>
2026-03-19 19:26:29 +0900

    change: 必要書類の文言追加

--- fe/src/components/pages/PackingList.tsx
+++ fe/src/components/pages/PackingList.tsx
@@ -4,18 +4,33 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 type PackingItem = {
   name: string;
+  content: string;
   path: string;
 };
 
 export const PackingList = () => {
   const itemList: PackingItem[] = [
-    { name: "パスポート", path: "/usa/packing-list/pasport" },
-    { name: "ESTA(エスタ)", path: "/usa/packing-list/esta" },
+    {
+      name: "パスポート",
+      content: "残存期間は帰国日まで有効なもの。IC旅券(e-passport)であること。",
+      path: "/usa/packing-list/pasport",
+    },
+    {
+      name: "ESTA(エスタ)",
+      content:
+        "オンラインで申請。40ドル。渡航前(推奨72時間前)までに申請・承認が必要。",
+      path: "/usa/packing-list/esta",
+    },
     {
       name: "往復または次の目的地への航空券",
+      content: "滞在終了時にアメリカを出国する航空券。",
       path: "/usa/packing-list/ticket",
     },
-    { name: "税関申告書", path: "/usa/packing-list/tax" },
+    {
+      name: "税関申告書",
+      content: "機内で配られる。またはデジタル(MCPアプリ等)で申請。",
+      path: "/usa/packing-list/tax",
+    },
   ];
   return (
     <HeaderLayout>
@@ -30,7 +45,9 @@ export const PackingList = () => {
           持ち物Make changes to your account here.
           {itemList.map((item) => (
             <div key={item.name}>
-              <div>・{item.name}</div>
+              <div>
+                ・{item.name}:{item.content}
+              </div>
               <div>
                 <Link to={item.path}>詳細確認</Link>
               </div>
