14641d80bb5d5236e3a50eab438f0d89f694e622
keikokubu <k.kokubu@scsk.jp>
2026-03-19 16:30:00 +0900

    create: packing-listページ

---
+++ fe/src/components/pages/PackingList.tsx
@@ -0,0 +1,9 @@
+import { HeaderLayout } from "../templetes/HeaderLayout";
+
+export const PackingList = () => {
+  return (
+    <HeaderLayout>
+      <div>アメリカの持ち物</div>
+    </HeaderLayout>
+  );
+};
--- fe/src/router/Router.tsx
+++ fe/src/router/Router.tsx
@@ -3,6 +3,7 @@ import { AuthContextProvider } from "@/contexts/AuthContexts";
 import { LoginPage } from "@/components/pages/LoginPage";
 import { SignUpPage } from "@/components/pages/SignUpPage";
 import { Home } from "@/components/pages/Home";
+import { PackingList } from "@/components/pages/PackingList";
 // import { PrivateRoute } from "@/components/templetes/PrivateRoute";
 
 export const Router = () => {
@@ -19,6 +20,7 @@ export const Router = () => {
         ></Route>
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/packing-list" element={<PackingList />} />
       </Routes>
     </AuthContextProvider>
   );
