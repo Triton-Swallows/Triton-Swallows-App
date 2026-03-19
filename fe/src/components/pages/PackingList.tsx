3e108e008d5b3a51ef39c97c9433374eba6f027f
keikokubu <k.kokubu@scsk.jp>
2026-03-19 19:13:33 +0900

    add: 詳細確認ボタン

--- fe/src/components/pages/PackingList.tsx
+++ fe/src/components/pages/PackingList.tsx
@@ -1,12 +1,18 @@
+import { Link } from "react-router-dom";
 import { HeaderLayout } from "../templetes/HeaderLayout";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
+type PackingItem = {
+  name: string;
+  path: string;
+};
+
 export const PackingList = () => {
-  const itemList = [
-    "パスポート",
-    "ESTA(エスタ)",
-    "往復または次の目的地への航空券",
-    "税関申告書",
+  const itemList: PackingItem[] = [
+    { name: "パスポート", path: "/pasport" },
+    { name: "ESTA(エスタ)", path: "/esta" },
+    { name: "往復または次の目的地への航空券", path: "/ticket" },
+    { name: "税関申告書", path: "/tax" },
   ];
   return (
     <HeaderLayout>
@@ -20,7 +26,12 @@ export const PackingList = () => {
           <h2>必要書類</h2>
           持ち物Make changes to your account here.
           {itemList.map((item) => (
-            <div key={item}>・{item}</div>
+            <div key={item.name}>
+              <div>・{item.name}</div>
+              <div>
+                <Link to={item.path}>詳細確認</Link>
+              </div>
+            </div>
           ))}
         </TabsContent>
         <TabsContent value="review">
