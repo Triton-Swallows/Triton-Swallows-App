eaee88844483e4b5e2863232c84eb76ca43b70a5
naoki <jera.hq.devtr.mem009@outlook.jp>
2026-03-19 16:31:20 +0900

    add: トップページに旅行を準備するボタンを追加

--- fe/src/components/pages/Home.tsx
+++ fe/src/components/pages/Home.tsx
@@ -1,11 +1,14 @@
-import { RiHome3Line } from "react-icons/ri";
+import { Link } from "react-router-dom";
 import { HeaderLayout } from "../templetes/HeaderLayout";
+import { Button } from "../ui/button";
 
 export const Home = () => {
   return (
     <HeaderLayout>
       <div className="flex justify-center items-center h-screen text-[10rem]">
-        <RiHome3Line />
+        <Link to="/">
+          <Button> 旅行を準備する </Button>
+        </Link>
       </div>
     </HeaderLayout>
   );
