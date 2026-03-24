be6509f099fb5231761c4e9e5c03b3dc344da410
naoki <jera.hq.devtr.mem009@outlook.jp>
2026-03-24 14:48:58 +0900

    add: reviewテーブルにcountry_name列を追加

---
+++ be/db/migrations/20260324053717_alter_review_table.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.alterTable("reviews", function (table) {
+    table.string("country_name");
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.alterTable("reviews", function (table) {
+    table.dropColumn("country_name");
+  });
+};
