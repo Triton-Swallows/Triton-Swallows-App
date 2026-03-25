b453c7a1acda49da9bccc305075ec80918b693ff
naoki <jera.hq.devtr.mem009@outlook.jp>
2026-03-25 09:47:42 +0900

    add: 国ごとの口コミ(reviews)を取得するAPIの実装

--- be/src/routes/review.js
+++ be/src/routes/review.js
@@ -4,14 +4,15 @@ const { verifyToken } = require("../middlewares/auth.middlewares");
 function createReviewRouter(reviewsController) {
   const router = express.Router();
 
-  //  TODO: verify tokenを追加
   // 口コミを全件取得
   router.get("/reviews", verifyToken, reviewsController.getAll);
 
-  // // 国ごとの口コミを全権取得
-  // router.get(
-  //   "/reviews/:countryName", reviewsController.getByCountry,
-  // );
+  // 国ごとの口コミを全権取得
+  router.get(
+    "/reviews/:countryName",
+    verifyToken,
+    reviewsController.getByCountry,
+  );
 
   return router;
 }
