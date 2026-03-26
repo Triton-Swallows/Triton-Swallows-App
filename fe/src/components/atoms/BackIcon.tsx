fb9d99d360ae4c771065b9d8b340a077d2757be8
naoki <jera.hq.devtr.mem009@outlook.jp>
2026-03-26 15:24:45 +0900

    style: backIconのラベルのデザイン修正(改行を禁止)
    Co-authored-by: kei-kokubu <kei-kokubu@users.noreply.github.com>

--- fe/src/components/atoms/BackIcon.tsx
+++ fe/src/components/atoms/BackIcon.tsx
@@ -13,7 +13,7 @@ export const BackIcon: React.FC<Props> = ({ path, label }) => {
       className="flex items-center bg-[#FFD6B3] w-[103px] h-[56px] px-[3px] py-[10px] justify-center rounded-r"
     >
       <RiArrowLeftWideLine className="text-4xl relative right-[5px]" />
-      <span className="flex items-center w-[85px] h-[24px] text-[12px] relative right-[17px] font-medium">
+      <span className="flex items-center w-[102px] h-[24px] text-[12px] relative right-[13px] font-medium whitespace-nowrap">
         {label}
       </span>
     </Link>
