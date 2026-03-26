2ebf0a2557d801277af91f75eae64a90544218ab
NatsumiMiyamotoJeraTr <Jera.hq.devtr.mem010@outlook.jp>
2026-03-26 17:32:55 +0900

    fix: 不要コンポーネントの記載削除

--- fe/src/components/pages/EstaPage.tsx
+++ fe/src/components/pages/EstaPage.tsx
@@ -1,6 +1,5 @@
 import { useState, useEffect } from "react";
 import { HeaderLayout } from "../templetes/HeaderLayout";
-import { BackIcon } from "../atoms/BackIcon";
 import {
   Accordion,
   AccordionContent,
