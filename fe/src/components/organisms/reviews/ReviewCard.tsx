fa953122c790ccfe49bf1f755a80110e53d44bb9
naoki <jera.hq.devtr.mem009@outlook.jp>
2026-03-26 14:27:55 +0900

    fix: reviwCardの条件式の修正

--- fe/src/components/organisms/reviews/ReviewCard.tsx
+++ fe/src/components/organisms/reviews/ReviewCard.tsx
@@ -49,7 +49,7 @@ export const ReviewCard: React.FC<Props> = ({
         <div className="flex justify-end gap-2">
           <span>{like_count}</span>
           <button
-            className={`flex items-center ${!!loginUser ? "cursor-pointer" : "cursor-not-allowed"}`}
+            className={`flex items-center ${loginUser ? "cursor-pointer" : "cursor-not-allowed"}`}
             onClick={() => onToggleLike(review_id, liked_by_me)}
             disabled={!loginUser}
           >
