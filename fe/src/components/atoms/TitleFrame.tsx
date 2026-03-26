fedc06365c1406ccc69b6500d775c3e0f153232f
NatsumiMiyamotoJeraTr <Jera.hq.devtr.mem010@outlook.jp>
2026-03-26 11:35:22 +0900

    fix: 幅指定を削除

--- fe/src/components/atoms/TitleFrame.tsx
+++ fe/src/components/atoms/TitleFrame.tsx
@@ -6,7 +6,7 @@ type Props = {
 
 export const TitleFrame: React.FC<Props> = ({ title, date, superivisor }) => {
   return (
-    <div className="text-[#FAF6F0] bg-[#2D8AB7] w-[393px] h-[48px] flex justify-between items-center">
+    <div className="text-[#FAF6F0] bg-[#2D8AB7] h-[48px] flex justify-between items-center">
       <div className="font-bold text-[20px] px-[8px]">{title}</div>
       <div className="flex-col self-start text-[10px] px-[8px]">
         <div className="justify-self-end">{date}</div>
