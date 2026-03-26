10895441de4588fe6f5697c24bb84f3b4cbc0862
naoki <jera.hq.devtr.mem009@outlook.jp>
2026-03-26 15:13:35 +0900

    style: country pageのスタイル適用

--- fe/src/components/pages/CountryPage.tsx
+++ fe/src/components/pages/CountryPage.tsx
@@ -7,7 +7,11 @@ export const CountryPage = () => {
     <HeaderLayout>
       <HeaderNav path={"/"} label="トップページ" title="国リスト" />
       <Link to="/usa/packing-list">
-        <div>アメリカ</div>
+        <div className="bg-[#A8C9DE]  flex justify-center item-center h-[72px] py-[25px] px-[12px] rounded-xl mx-[16px]">
+          <span className="text-[16px] font-midium text-[#002B45]">
+            アメリカ（米国）
+          </span>
+        </div>
       </Link>
     </HeaderLayout>
   );
