1a588f82828c6dc98b7463bab47a399015c8ea03
NatsumiMiyamotoJeraTr <Jera.hq.devtr.mem010@outlook.jp>
2026-03-30 15:10:42 +0900

    feat: 複数の国が選べるよう追加

--- fe/src/components/pages/CountryPage.tsx
+++ fe/src/components/pages/CountryPage.tsx
@@ -13,6 +13,28 @@ export const CountryPage = () => {
           </span>
         </div>
       </Link>
+      <Link to="/korea/packing-list">
+        <div className="bg-[#A8C9DE]  flex justify-center item-center h-[72px] py-[25px] px-[12px] rounded-xl mx-[16px]">
+          <span className="text-[16px] font-midium text-[#002B45]">韓国</span>
+        </div>
+      </Link>
+      <Link to="/taiwan/packing-list">
+        <div className="bg-[#A8C9DE]  flex justify-center item-center h-[72px] py-[25px] px-[12px] rounded-xl mx-[16px]">
+          <span className="text-[16px] font-midium text-[#002B45]">台湾</span>
+        </div>
+      </Link>
+      <Link to="/thailand/packing-list">
+        <div className="bg-[#A8C9DE]  flex justify-center item-center h-[72px] py-[25px] px-[12px] rounded-xl mx-[16px]">
+          <span className="text-[16px] font-midium text-[#002B45]">タイ</span>
+        </div>
+      </Link>
+      <Link to="/vietnam/packing-list">
+        <div className="bg-[#A8C9DE]  flex justify-center item-center h-[72px] py-[25px] px-[12px] rounded-xl mx-[16px]">
+          <span className="text-[16px] font-midium text-[#002B45]">
+            ベトナム
+          </span>
+        </div>
+      </Link>
     </HeaderLayout>
   );
 };
