00b9678b3c811463a731811730f0586c81e65abd
naoki <jera.hq.devtr.mem009@outlook.jp>
2026-04-01 11:01:22 +0900

    add: 開発中の詳細ページを追加（パスポート、航空券、税関）

---
+++ fe/src/components/pages/TicketPage.tsx
@@ -0,0 +1,23 @@
+import { HeaderLayout } from "../templetes/HeaderLayout";
+import { TitleFrame } from "../atoms/TitleFrame";
+import { SummaryFrameDetail } from "../atoms/SummaryFrameDetail";
+import { HeaderNav } from "../molecules/HeaderNav";
+
+export const TicketPage = () => {
+  return (
+    <HeaderLayout>
+      <HeaderNav
+        path={"/usa/packing-list"}
+        label="持ち物/口コミ"
+        title="アメリカ（米国）"
+      />
+      <section>
+        <TitleFrame title="航空券とは:" date="" superivisor="" />
+        <SummaryFrameDetail
+          contents="こちら現在開発中になっております。しばらくお待ちください"
+          notes=""
+        />
+      </section>
+    </HeaderLayout>
+  );
+};
