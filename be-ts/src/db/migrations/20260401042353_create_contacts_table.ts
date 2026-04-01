37fc222480d0155eef05ce3a561cf91735727f79
naoki <jera.hq.devtr.mem009@outlook.jp>
2026-04-01 13:45:22 +0900

    add: contactsテーブルのmigrationファイル実装
    Co-authored-by: kei-kokubu <kei-kokubu@users.noreply.github.com>
    Co-authored-by: nm2443 <nm2443@users.noreply.github.com>

---
+++ be-ts/src/db/migrations/20260401042353_create_contacts_table.ts
@@ -0,0 +1,21 @@
+import type { Knex } from "knex";
+
+export async function up(knex: Knex): Promise<void> {
+  return knex.schema.createTable("contacts", function (table) {
+    table.increments("id").primary();
+    table.string("user_id").notNullable();
+    table.foreign("user_id").references("users.uid").onDelete("CASCADE");
+    table.string("email", 255).notNullable();
+    table.foreign("email").references("users.email").onDelete("CASCADE");
+    table.string("target").notNullable();
+    table.text("description").notNullable();
+    table.text("others");
+    table.boolean("is_checked").notNullable().defaultTo(false);
+    table.boolean("is_accepted").notNullable();
+    table.timestamps(true, true);
+  });
+}
+
+export async function down(knex: Knex): Promise<void> {
+  return knex.schema.dropTable("contacts");
+}
