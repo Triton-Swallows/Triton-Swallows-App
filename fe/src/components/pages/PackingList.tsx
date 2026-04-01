e84990fe359c2cbf383f88a6b35e0438bdf46395
naoki <jera.hq.devtr.mem009@outlook.jp>
2026-04-01 11:03:04 +0900

    add: 開発中の詳細ページへ持ち物ページからアクセスできるように実装

--- fe/src/components/pages/PackingList.tsx
+++ fe/src/components/pages/PackingList.tsx
@@ -16,25 +16,25 @@ export const PackingList = () => {
 
   const itemList: PackingItem[] = [
     {
-      name: "パスポート",
-      content: "残存期間は帰国日まで有効なもの。IC旅券(e-passport)であること。",
-      path: "",
-    },
-    {
       name: "ESTA(エスタ)",
       content:
         "オンラインで申請。40ドル。渡航前(推奨72時間前)までに申請・承認が必要。",
       path: "/usa/packing-list/esta",
     },
     {
+      name: "パスポート",
+      content: "残存期間は帰国日まで有効なもの。IC旅券(e-passport)であること。",
+      path: "/usa/packing-list/passport",
+    },
+    {
       name: "往復または次の目的地への航空券",
       content: "滞在終了時にアメリカを出国する航空券。",
-      path: "",
+      path: "/usa/packing-list/ticket",
     },
     {
       name: "税関申告書",
       content: "機内で配られる。またはデジタル(MCPアプリ等)で申請。",
-      path: "",
+      path: "/usa/packing-list/tax",
     },
   ];
 
