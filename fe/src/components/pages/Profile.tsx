aef71f9b51ede13f29510fb15856a4b22f12e9ad
naoki <jera.hq.devtr.mem009@outlook.jp>
2026-04-02 14:16:43 +0900

    add: プロフィール画像変更時にヘッダーのアイコン画像も変化

--- fe/src/components/pages/Profile.tsx
+++ fe/src/components/pages/Profile.tsx
@@ -1,49 +1,15 @@
-import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { HeaderLayout } from "../templetes/HeaderLayout";
 import { HeaderNav } from "../molecules/HeaderNav";
-import apiClient from "@/config/apiClient";
 import { AuthContextConsumer } from "@/contexts/AuthContexts";
 import defalutlProfileIcon from "../../assets/UserIcon.png";
 import { Button } from "../ui/button";
 import { EditProfileDialog } from "../organisms/dialogs/EditProfileDialog";
 
-type User = {
-  ud: string;
-  user_name: string;
-  email: string;
-  icon_url: string;
-  count: number; //累計投稿数
-  total_like_count: number; //累計いいね
-  total_point: number; //累計ポイント数
-  consume_point: number; //消費ポイント
-  my_point: number; //残高
-};
-
-type ApiResponse<T> = {
-  data: T;
-};
-
 export const Profile = () => {
-  const { loginUser, loading, logout } = AuthContextConsumer();
-  const [user, setUser] = useState<User | null>(null);
+  const { loginUser, loading, userInfo, logout, refreshUserInfo } =
+    AuthContextConsumer();
   const navigate = useNavigate();
-
-  const fetchProfile = async () => {
-    if (!loginUser) return;
-    try {
-      // ログイン中の自分の情報（User）を取得
-      const response = await apiClient.get<ApiResponse<User>>("/users/me");
-      const userData = response.data.data;
-      setUser(userData);
-    } catch (error) {
-      console.error("ユーザー情報の取得に失敗しました。", error);
-    }
-  };
-
-  useEffect(() => {
-    fetchProfile();
-  }, [loginUser]);
 
   const handleLogout = async (): Promise<void> => {
     try {
@@ -61,7 +27,7 @@ export const Profile = () => {
       <div>
         <label>プロフィール画像</label>
         <img
-          src={user?.icon_url || defalutlProfileIcon}
+          src={userInfo?.icon_url || defalutlProfileIcon}
           alt="プロフィールアイコン"
           className="w-[120px] h-[120px] rounded-full"
         />
@@ -69,31 +35,31 @@ export const Profile = () => {
       {/* ユーザー情報 */}
       <div className="flex gap-[10px]">
         <label>アカウント名:</label>
-        <p>{user?.user_name}</p>
+        <p>{userInfo?.user_name}</p>
       </div>
       <div className="flex gap-[10px]">
         <label>メールアドレス:</label>
-        <p>{user?.email}</p>
+        <p>{userInfo?.email}</p>
       </div>
       <div className="flex gap-[10px]">
         <label>口コミ投稿数:</label>
-        <p>{user?.count}</p>
+        <p>{userInfo?.review_count}</p>
       </div>
       <div className="flex gap-[10px]">
         <label>累計いいね数:</label>
-        <p>{user?.total_like_count}</p>
+        <p>{userInfo?.total_like_count}</p>
       </div>
       <div className="flex gap-[10px]">
         <label>累計ポイント数:</label>
-        <p>{user?.total_point} pt</p>
+        <p>{userInfo?.total_point} pt</p>
       </div>
       <div className="flex gap-[10px]">
         <label>消費ポイント:</label>
-        <p>{user?.consume_point} pt</p>
+        <p>{userInfo?.consume_point} pt</p>
       </div>
       <div className="flex gap-[10px]">
         <label>残高:</label>
-        <p>{user?.my_point} pt</p>
+        <p>{userInfo?.my_point} pt</p>
       </div>
 
       <Button
@@ -105,7 +71,7 @@ export const Profile = () => {
         ログアウト
       </Button>
 
-      <EditProfileDialog user={user} onUpdate={fetchProfile} />
+      <EditProfileDialog user={userInfo} onUpdate={refreshUserInfo} />
     </HeaderLayout>
   );
 };
