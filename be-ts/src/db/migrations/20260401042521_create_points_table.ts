18c43cc43c46be4074ede1daa3708b994a239685
naoki <jera.hq.devtr.mem009@outlook.jp>
2026-04-02 10:35:57 +0900

    fix: pointテーブルのbonus列のタイポ修正

--- be-ts/src/db/migrations/20260401042521_create_points_table.ts
+++ be-ts/src/db/migrations/20260401042521_create_points_table.ts
@@ -4,7 +4,7 @@ export async function up(knex: Knex): Promise<void> {
   return knex.schema.createTable("points", function (table) {
     table.increments("id").primary();
     table.string("user_id").notNullable();
-    table.integer("bounus_point").notNullable().defaultTo(0);
+    table.integer("bonus_point").notNullable().defaultTo(0);
     table.integer("consume_point").notNullable().defaultTo(0);
     table.integer("total_point").notNullable().defaultTo(0);
     table.timestamps(true, true);
--- be-ts/src/db/seeds/08_points.ts
+++ be-ts/src/db/seeds/08_points.ts
@@ -5,19 +5,19 @@ export async function seed(knex: Knex): Promise<void> {
   await knex("points").insert([
     {
       user_id: "tsJF90ELf6YFtJuc9ac247khhr02",
-      bounus_point: 200,
+      bonus_point: 200,
       consume_point: 0,
       total_point: 206,
     },
     {
       user_id: "5A2LwTI4C3WMzWLOi80HoY6oyQB2",
-      bounus_point: 0,
+      bonus_point: 0,
       consume_point: 0,
       total_point: 2,
     },
     {
       user_id: "9HLcP3zl5ifAqniVlfz0Qp62xE63",
-      bounus_point: 0,
+      bonus_point: 0,
       consume_point: 0,
       total_point: 2,
     },
