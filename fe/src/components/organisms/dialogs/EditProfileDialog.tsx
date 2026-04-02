9ef24a1eb37f220ee3ad41203509fa93e0be8221
naoki <jera.hq.devtr.mem009@outlook.jp>
2026-04-02 14:17:16 +0900

    add: プロフィール修正ダイアログのplaceholderの修正

--- fe/src/components/organisms/dialogs/EditProfileDialog.tsx
+++ fe/src/components/organisms/dialogs/EditProfileDialog.tsx
@@ -9,7 +9,7 @@ import {
   DialogTrigger,
 } from "@/components/ui/dialog";
 import apiClient from "@/config/apiClient";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 type EditProfileDialogProps = {
   user: {
@@ -27,6 +27,13 @@ export const EditProfileDialog = ({
   // const [iconUrl, setIconUrl] = useState(user.icon_url);
   const [iconFile, setIconFile] = useState<File | null>(null);
   const [open, setOpen] = useState(false);
+
+  useEffect(() => {
+    if (open && user) {
+      setName(user.user_name);
+      setIconFile(null); // ファイル選択もリセット
+    }
+  }, [open, user]);
 
   const API_KEY = import.meta.env.VITE_IMAGEBB_API_KEY;
 
