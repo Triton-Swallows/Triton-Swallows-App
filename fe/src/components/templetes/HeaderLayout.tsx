07db4d6593fb201f9f1055aef17228344269c440
naoki <jera.hq.devtr.mem009@outlook.jp>
2026-04-03 22:17:55 +0900

    style: テンプレートにhedaerの修正を反映。テンプレートから、ヘッダーの表示内容をすべて操作する仕様。

--- fe/src/components/templetes/HeaderLayout.tsx
+++ fe/src/components/templetes/HeaderLayout.tsx
@@ -1,15 +1,24 @@
 import { Footer } from "../organisms/layout/Footer";
-import { NavBar } from "../organisms/layout/NavBar";
+// import { NavBar } from "../organisms/layout/NavBar";
+import { Header } from "../organisms/layout/Header";
 
 type Props = {
   children: React.ReactNode;
+  title?: string;
+  showBackButton?: boolean;
+  transparent?: boolean;
+  backgroundImage?: string;
+  path?: string;
 };
 
-export const HeaderLayout: React.FC<Props> = ({ children }) => {
+export const HeaderLayout: React.FC<Props> = ({ children, ...headerProps }) => {
   return (
     <div className="flex flex-col h-screen overflow-hidden">
-      <NavBar />
-      <main className="flex-1 overflow-auto pb-24">{children}</main>
+      {/* <NavBar /> */}
+      <Header {...headerProps} />
+      <main className="flex-1 overflow-auto pb-[24px] pt-[56px]">
+        {children}
+      </main>
       <Footer />
     </div>
   );
