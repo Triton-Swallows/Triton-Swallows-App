d3791244b67563fc8c0c1a7f1398cb04e7a4ea2a
keikokubu <k.kokubu@scsk.jp>
2026-04-03 17:50:40 +0900

    add: button大枠

--- fe/src/components/pages/PackingCheckList.tsx
+++ fe/src/components/pages/PackingCheckList.tsx
@@ -1,5 +1,10 @@
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Button } from "../ui/button";
 export const PackingCheckList = () => {
+  const handleCreateList = () => {
+    console.log("押された!");
+  };
+
   return (
     <>
       <div>ここは新しい機能ページです</div>
@@ -11,6 +16,12 @@ export const PackingCheckList = () => {
         </TabsList>
         <TabsContent value="myList">
           <h2>ここはマイリスト</h2>
+          <Button
+            className="bg-[#00588C] text-[#FAF6F0]"
+            onClick={handleCreateList}
+          >
+            +新規リスト作成
+          </Button>
         </TabsContent>
         <TabsContent value="everyoneLine">
           <h2>ここはみんなのリスト</h2>
