4d918e0adc849197a69fc44de15525368201c70c
keikokubu <k.kokubu@scsk.jp>
2026-04-03 15:33:09 +0900

    add: tabsの実装

--- fe/src/components/pages/PackingCheckList.tsx
+++ fe/src/components/pages/PackingCheckList.tsx
@@ -1,8 +1,21 @@
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 export const PackingCheckList = () => {
   return (
     <>
       <div>ここは新しい機能ページです</div>
       <div>がんばるぞ</div>
+      <Tabs defaultValue="myList" className="flex-col">
+        <TabsList variant="line">
+          <TabsTrigger value="myList">マイリスト</TabsTrigger>
+          <TabsTrigger value="everyoneLine">みんなのリスト</TabsTrigger>
+        </TabsList>
+        <TabsContent value="myList">
+          <h2>ここはマイリスト</h2>
+        </TabsContent>
+        <TabsContent value="everyoneLine">
+          <h2>ここはみんなのリスト</h2>
+        </TabsContent>
+      </Tabs>
     </>
   );
 };
