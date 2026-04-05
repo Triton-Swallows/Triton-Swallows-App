d2e818205b6bf22278aadf701a7e5da92c3433c8
naoki <jera.hq.devtr.mem009@outlook.jp>
2026-04-05 20:02:22 +0900

    style: title frameに並び替えを表示

--- fe/src/components/atoms/TitleFrame.tsx
+++ fe/src/components/atoms/TitleFrame.tsx
@@ -2,16 +2,26 @@ type Props = {
   title: string;
   date?: string;
   superivisor?: string;
+  children?: React.ReactNode;
 };
 
-export const TitleFrame: React.FC<Props> = ({ title, date, superivisor }) => {
+export const TitleFrame: React.FC<Props> = ({
+  title,
+  date,
+  superivisor,
+  children,
+}) => {
   return (
     <div className="text-[#FAF6F0] bg-[#2D8AB7] h-[48px] flex justify-between items-center">
       <div className="font-bold text-[20px] px-[8px]">{title}</div>
-      <div className="flex-col self-start text-[10px] px-[8px]">
-        <div className="justify-self-end">{date}</div>
-        <div className="justify-self-end">{superivisor}</div>
-      </div>
+      <div className="flex-grow flex justify-end items-center">{children}</div>
+
+      {(date || superivisor) && (
+        <div className="flex-col self-start text-[10px] px-[8px]">
+          <div className="justify-self-end">{date}</div>
+          <div className="justify-self-end">{superivisor}</div>
+        </div>
+      )}
     </div>
   );
 };
