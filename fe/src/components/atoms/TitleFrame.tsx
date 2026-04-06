ceeb5ec5e61195cad0b5077c614f763d167788a4
naoki <jera.hq.devtr.mem009@outlook.jp>
2026-04-06 23:17:30 +0900

    style: 認証バッジの付与

--- fe/src/components/atoms/TitleFrame.tsx
+++ fe/src/components/atoms/TitleFrame.tsx
@@ -1,8 +1,11 @@
+import { RiVerifiedBadgeFill } from "react-icons/ri";
+
 type Props = {
   title: string;
   date?: string;
   superivisor?: string;
   children?: React.ReactNode;
+  badge?: boolean;
 };
 
 export const TitleFrame: React.FC<Props> = ({
@@ -10,6 +13,7 @@ export const TitleFrame: React.FC<Props> = ({
   date,
   superivisor,
   children,
+  badge,
 }) => {
   return (
     <div className="text-[#FAF6F0] bg-[#2D8AB7] h-[48px] flex justify-between items-center">
@@ -17,9 +21,19 @@ export const TitleFrame: React.FC<Props> = ({
       <div className="flex-grow flex justify-end items-center">{children}</div>
 
       {(date || superivisor) && (
-        <div className="flex-col self-start text-[10px] px-[8px]">
-          <div className="justify-self-end">{date}</div>
-          <div className="justify-self-end">{superivisor}</div>
+        <div className="flex-col self-start px-[8px] py-[5px] gap-[4px]">
+          <div className="justify-self-end text-[12px]">{date}</div>
+          <div className="justify-self-end gap-[6px] flex items-center text-[12px]">
+            {badge ? (
+              <RiVerifiedBadgeFill
+                color="#FFB800"
+                className="w-[20px] h-[20px]"
+              />
+            ) : (
+              <></>
+            )}
+            {superivisor}
+          </div>
         </div>
       )}
     </div>
