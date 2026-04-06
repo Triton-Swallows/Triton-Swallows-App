a1ab0d6933d17d7a9389107e0e254656ec49ce2e
naoki <jera.hq.devtr.mem009@outlook.jp>
2026-04-07 00:57:02 +0900

    style: action barの修正

--- fe/src/components/atoms/ActionBar.tsx
+++ fe/src/components/atoms/ActionBar.tsx
@@ -10,15 +10,19 @@ type Props = {
 export const ActionBar: React.FC<Props> = ({ actions }) => {
   return (
     <div className="text-[#F1F5F9] bg-[#2BA89D] h-[48px] w-full flex justify-between items-center">
-      {actions.map((action) => (
-        <button
-          key={action.label}
-          onClick={action.onClick}
-          className="text-[14px] font-medium px-[8px]"
-        >
-          {action.label}
-        </button>
-      ))}
+      {actions && actions.length > 0 ? (
+        actions.map((action) => (
+          <button
+            key={action.label}
+            onClick={action.onClick}
+            className="text-[14px] font-medium px-[8px]"
+          >
+            {action.label}
+          </button>
+        ))
+      ) : (
+        <div />
+      )}
     </div>
   );
 };
