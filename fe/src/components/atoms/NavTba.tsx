f62a600627ff2368dfd9624ad52524b0e2c89712
NatsumiMiyamotoJeraTr <Jera.hq.devtr.mem010@outlook.jp>
2026-04-06 17:33:19 +0900

    fix: デザインストーリー修正

--- fe/src/components/atoms/NavTba.tsx
+++ fe/src/components/atoms/NavTba.tsx
@@ -10,7 +10,7 @@ export const NavTab = ({ to, label, isActive }: NavTabProps) => {
   return (
     <Link
       to={to}
-      className={`flex items-center justify-center text-[22px] font-bold rounded-xl px-[20px] h-[45px]
+      className={`flex items-center justify-center text-[16px] font-bold rounded-xl px-[20px] h-[45px]
      ${isActive ? "bg-[#F1F5F9] text-[#002B45]" : "text-[#002B45]"}`}
     >
       {label}
