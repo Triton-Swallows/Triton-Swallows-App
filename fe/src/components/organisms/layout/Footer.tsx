ec9da4a6da4a150fb873ea2a5e14fad24149144e
NatsumiMiyamotoJeraTr <Jera.hq.devtr.mem010@outlook.jp>
2026-04-06 11:13:39 +0900

    fix: footerをすりガラス風に

--- fe/src/components/organisms/layout/Footer.tsx
+++ fe/src/components/organisms/layout/Footer.tsx
@@ -20,7 +20,7 @@ export const Footer = () => {
 
   return (
     <>
-      <footer className="fixed bottom-[4px] left-1/2 -translate-x-1/2 z-20 bg-white/50 text-[#00588C] py-4 shadow-lg h-[63px] w-[262px] md:w-[1200px] md:h-[80px] bottom-0 rounded-3xl">
+      <footer className="fixed bottom-[4px] left-1/2 -translate-x-1/2 z-20 backdrop-blur-md bg-white/50 text-[#00588C] py-4 shadow-lg h-[63px] w-[262px] md:w-[1200px] md:h-[80px] bottom-0 rounded-3xl">
         <div className="flex justify-around items-center px-4 h-full">
           <Link to="/" className=" hover:text-blue-100">
             <RiHome3Line className="w-[45px] h-[50px]" />
