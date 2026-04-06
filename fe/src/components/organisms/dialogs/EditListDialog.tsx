6583d361db8228cf18eee6e7683355e00242c675
keikokubu <k.kokubu@scsk.jp>
2026-04-06 15:01:12 +0900

    change: 不要importの削除

--- fe/src/components/organisms/dialogs/EditListDialog.tsx
+++ fe/src/components/organisms/dialogs/EditListDialog.tsx
@@ -8,7 +8,6 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
-import { Button } from "@/components/ui/button";
 import { EditListTitleDialog } from "./EditListTitileDialog";
 import { CheckDeleteItemDialog } from "./CheckDeleteItemDialog";
 
